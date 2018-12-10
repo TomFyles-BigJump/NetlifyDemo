ac28e59fab6ba0381f54b2b8db6f7491aac548df
TomFyles-BigJump <tom@bigjump.co.uk>
2018-12-10 10:03:46 +0000

    Add netlify.toml file

--- src/views/Users/list.vue
+++ src/views/Users/list.vue
@@ -75,7 +75,7 @@ export default {
 .card{
   box-shadow: 0 1px 5px rgba(0, 0, 0, .2), 0 2px 2px rgba(0, 0, 0, .14), 0 3px 1px -2px rgba(0, 0, 0, .12);
   padding: 5px;
-  margin-bottom: 20px;
+  margin: 20px;
 }
 
 .mt-10{
